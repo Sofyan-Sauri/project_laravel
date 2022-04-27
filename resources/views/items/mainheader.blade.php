fce5ad78f91523e6a978c915a6258b6f7b2f6fa2
sofyan-sauri <sofyan.sauri19@mhs.ubharajaya.ac.id>
2022-04-27 19:07:08 +0700

    sofyan

--- resources/views/items/mainheader.blade.php
+++ resources/views/items/mainheader.blade.php
@@ -37,13 +37,13 @@
 									<li class='dropdown user user-menu'>
 										<a href='#' class='dropdown-toggle' data-toggle='dropdown'>
 												<i class='glyphicon glyphicon-user'></i>
-												<span>Yanti <i class='caret'></i></span>
+												<span>Sofyan <i class='caret'></i></span>
 										</a>
 										<ul class='dropdown-menu'>
 											<!-- User image -->
 											<li class='user-header bg-light-blue'>
 													<img src='../foto_banner/foto.jpg' class='img-circle'/>
-													<p>Yanti</p>
+													<p>Sofyan</p>
 											</li>
 											<li class='user-footer'>
 												<div class='pull-left'>
